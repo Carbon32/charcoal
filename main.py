dccff894859c6572b81aff3fb6f0e807e44f4615
Carbon32 <contact.carbonofficial@gmail.com>
2022-01-08 01:05:44 +0100

    Added syntax highlighting (placeholder) and a few minor changes

--- main.py
+++ main.py
@@ -9,16 +9,29 @@
 
 from tkinter import *
 from tkinter.filedialog import askopenfilename, asksaveasfilename
+from idlelib.percolator import Percolator
+from idlelib.colorizer import ColorDelegator
+import subprocess
 
 # Editor Window: #
 
 window = Tk()
 window.title("Py Editor:")
+screenWidth = window.winfo_screenwidth()
+screenHeight = window.winfo_screenheight()
+window.geometry(f'{screenWidth // 2}x{screenHeight // 2}')
 
 # Text Editor: #
 
 textEditor = Text()
-textEditor.pack()
+textEditor.pack(side = "top", fill = "both", expand = True, padx = 0, pady = 0)
+
+# Output:
+
+outPut = Text(height = 7)
+outPut.pack(side = "bottom", fill = "both", expand = True, padx = 0, pady = 0)
+
+Percolator(textEditor).insertfilter(ColorDelegator()) # Placeholder
 
 # Editor Variables: #
 
@@ -27,8 +40,19 @@ globalPath = ''
 # Editor Functions: #
 
 def runCode():
-	code = textEditor.get('1.0', END)
-	exec(code)
+	global globalPath
+	if(globalPath == ''):
+		saveAsFile()
+	else:
+		saveFile()
+		outPut.delete('1.0', END)
+		command = f'python {globalPath}'
+		process = subprocess.Popen(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+		result, error = process.communicate()
+		if(error):
+			outPut.insert('1.0', error)
+		else:
+			outPut.insert('1.0', result)
 
 def newFile():
 	global globalPath
@@ -53,7 +77,6 @@ def saveFile():
 		with open(path, 'w') as file:
 			text = textEditor.get('1.0', END)
 			file.write(text)
-
 
 def saveAsFile():
 	global globalPath
