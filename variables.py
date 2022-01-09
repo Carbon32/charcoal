daad5b7395df7b7409ede5a877eb1336ec8b9f12
Carbon32 <contact.carbonofficial@gmail.com>
2022-01-09 21:45:02 +0100

    Added missing variables.py file

---
+++ variables.py
@@ -0,0 +1,16 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#													   		#
+#			           Editor Variables					    #
+#			          Developer: Carbon				       	#
+#													   		#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+textColor = '#FFFFFF'
+cursorColor = '#FFFFFF'
+backgroundColor = '#2D3132'
+cmntColor = '#BACBE7'
+keyWordColor = '#5FD66B'
+builtInColor = '#F29020'
+stringColor = '#1E69EB'
+buttonColor = '#1aff66'
+tabSize = 40
